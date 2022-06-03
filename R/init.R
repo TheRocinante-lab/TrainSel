5495d0abcc7bc37a24c37226777cfc059236f2d9
TheRocinante-lab <82779206+TheRocinante-lab@users.noreply.github.com>
2022-06-03 15:25:36 +0200

    Create init.R

---
+++ R/init.R
@@ -0,0 +1,12 @@
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("")
+  packageStartupMessage("***********************************************************")
+  packageStartupMessage("")
+  packageStartupMessage("     This is 'TrainSel' package, v 2.0")
+  packageStartupMessage("Copyright 2022 StatGen. All rights reserved.")
+  packageStartupMessage("Please read the LICENSE file for more information ")
+  packageStartupMessage("***********************************************************")
+}
+
+
+
